228b3d56a9dbd1479d06971c5e1c15b768255477
Poenaru Octavian <poenaruoctavian96@yahoo.com>
2018-01-28 01:02:13 +0200

    Products table created

---
+++ backend/src/migrations/20180127230137-create-products.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_product: {
+        type: Sequelize.INTEGER
+      },
+      id_invoice: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.DOUBLE
+      },
+      product_type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('products');
+  }
+};
---
+++ backend/src/models/products.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var products = sequelize.define('products', {
+    id_product: DataTypes.INTEGER,
+    id_invoice: DataTypes.INTEGER,
+    price: DataTypes.DOUBLE,
+    product_type: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return products;
+};
